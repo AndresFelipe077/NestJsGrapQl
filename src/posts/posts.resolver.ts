6cff1c15c27a37152f795dec019acce98c33d140
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-06-19 20:06:05 -0500

    Se sube ejercicio

---
+++ src/posts/posts.resolver.ts
@@ -0,0 +1,20 @@
+import { Resolver, Query } from '@nestjs/graphql';
+import { PostsService } from './posts.service';
+import { Post } from './post.entity';
+
+
+@Resolver()
+export class PostsResolver {
+
+
+  constructor(
+    private postsService: PostsService,
+  ){}
+
+  @Query((returns) => [Post])
+  posts() {
+    return this.postsService.findAll();
+  }
+
+
+}
---
+++ src/posts/posts.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { Post } from './post.entity';
+
+@Injectable()
+export class PostsService {
+
+
+  findAll(): Post[] {
+    return [
+      {
+        id: 1,
+        title: 'Hptaa',
+        content: 'aaa',
+      }
+    ];
+  }
+
+}
